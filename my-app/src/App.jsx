b10bf92d6b1a2c1b077bdc1eada89662ca95e17b
tierney03 <tierney_03@mac.com>
2022-12-17 18:12:23 -0600

    Adding Syntax

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import Header from "./components/header/header.jsx";
 import Nav from "./components/nav/nav.jsx";
 import About from "./components/about/about.jsx";
@@ -9,15 +8,15 @@ import Footer from "./components/footer/footer.jsx";
 
 const App = () => {
   return (
-    <>
-      <Header>Header</Header>
-      <Nav>Nav</Nav>
-      <About>About</About>
-      <Experience>Experience</Experience>
-      <Portfolio>Portfolio</Portfolio>
-      <Contact>Contact</Contact>
-      <Footer>Footer</Footer>
-    </>
+    <div>
+      <Header />
+      <Nav />
+      <About />
+      <Experience />
+      <Portfolio />
+      <Contact />
+      <Footer />
+    </div>
   );
 };
 
---
+++ my-app/src/components/portfolio/portfolio.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./portfolio.css";
+
+function portfolio() {
+  return <div>portfolio</div>;
+}
+
+export default portfolio;
