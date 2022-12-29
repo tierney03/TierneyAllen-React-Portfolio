d96abf0a64f73aed91a9a0f4376d5a05caca19d4
tierney03 <tierney_03@mac.com>
2022-12-29 11:27:56 -0500

    background image

--- my-app/src/components/navbar.js
+++ my-app/src/components/navbar.js
@@ -6,28 +6,30 @@ import NavDropdown from "react-bootstrap/NavDropdown";
 
 function Navigation() {
   return (
-    <Navbar bg="light" expand="lg">
-      <Container>
-        <Navbar.Brand href="#home">Tierney Allen</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <NavDropdown title="More Information" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#projects">Projects</NavDropdown.Item>
-              <NavDropdown.Item href="#skills">Skills</NavDropdown.Item>
-              <NavDropdown.Item href="#github">Github</NavDropdown.Item>
-              <NavDropdown.Item
-                href="https://drive.google.com/file/d/1MPI1pmrpXh0Z4_EgQkJEKCkt7Wsov4zT/view?usp=sharing"
-                target="_blank"
-              >
-                Resume
-              </NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+    <div class="nav">
+      <Navbar bg="light" expand="lg">
+        <Container>
+          <Navbar.Brand href="#home">Tierney Allen</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <NavDropdown title="More Information" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#projects">Projects</NavDropdown.Item>
+                <NavDropdown.Item href="#skills">Skills</NavDropdown.Item>
+                <NavDropdown.Item href="#github">Github</NavDropdown.Item>
+                <NavDropdown.Item
+                  href="https://drive.google.com/file/d/1MPI1pmrpXh0Z4_EgQkJEKCkt7Wsov4zT/view?usp=sharing"
+                  target="_blank"
+                >
+                  Resume
+                </NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </div>
   );
 }
 
