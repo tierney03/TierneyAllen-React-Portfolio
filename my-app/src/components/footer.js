2641807f5caad58af485a036f0f83b20c82c32f9
tierney03 <tierney_03@mac.com>
2023-01-01 11:05:57 -0500

    Adding icons for contacting via socials

--- my-app/src/components/footer.js
+++ my-app/src/components/footer.js
@@ -1,9 +1,26 @@
 import React, { useState } from "react";
+import { SiGithub } from "react-icons/si";
+import { SiLinkedin } from "react-icons/si";
+import { RiMailLine } from "react-icons/ri";
 
 function Footer() {
   return (
     <div>
-      <h1>FOOT</h1>
+      <h3>
+        <a href="https://github.com/tierney03" target="_blank">
+          <SiGithub />
+        </a>
+      </h3>
+      <h3>
+        <a href="https://www.linkedin.com/in/tierney-allen/" target="_blank">
+          <SiLinkedin />
+        </a>
+      </h3>
+      <h3>
+        <a href="mailto:tierney_03@mac.com" target="_blank">
+          <RiMailLine />
+        </a>
+      </h3>
     </div>
   );
 }
